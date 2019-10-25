64769029cb2685510cd31c0c9f9eab960cca163d
Tyler Ruggles <tylerhruggles@gmail.com>
2019-10-25 15:16:30 -0700

    First simple slides for use with SEM-1.2 and my fuels branch

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Simple_LaTeX
 For LaTeX slides to clone into other existing repositories
+
+
+
+## Cleaning Intermediat Files
+To remove the excess LaTeX files:
+
+```
+./clean.sh
+```
---
+++ clean.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+rm simple_beamer_slides_fuels.aux
+rm simple_beamer_slides_fuels.log
+rm simple_beamer_slides_fuels.nav
+rm simple_beamer_slides_fuels.out
+rm simple_beamer_slides_fuels.pdf
+rm simple_beamer_slides_fuels.snm
+rm simple_beamer_slides_fuels.synctex
+rm simple_beamer_slides_fuels.synctex.gz
+rm simple_beamer_slides_fuels.toc
+rm simple_beamer_slides_fuels.vrb
