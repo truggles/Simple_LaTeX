054b2cb54619405c237306d860b54ccf59749a5d
Tyler Ruggles <tylerhruggles@gmail.com>
2019-10-30 10:40:55 -0700

    Don't remove pdf

--- clean.sh
+++ clean.sh
@@ -4,7 +4,6 @@ rm simple_beamer_slides_fuels.aux
 rm simple_beamer_slides_fuels.log
 rm simple_beamer_slides_fuels.nav
 rm simple_beamer_slides_fuels.out
-rm simple_beamer_slides_fuels.pdf
 rm simple_beamer_slides_fuels.snm
 rm simple_beamer_slides_fuels.synctex
 rm simple_beamer_slides_fuels.synctex.gz
